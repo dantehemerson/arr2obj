6452ae7224a0148fa67970d18059dc6a3a21e58e
Dante Calderon <mail@dantecalderon.dev>
2019-08-30 18:35:10 -0500

    Receive key and value custom function

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,43 @@
-export const arrayToObject = (array: Array<any>, keyField?: string) => {
-  if (!Array.isArray(array) || !array.length) {
+declare interface Options {
+  key?: Function
+  value?: Function
+}
+
+/**
+ * Validate an option key.
+ * @param optionKey should be one key of Option(ejm: key, value)
+ */
+function getOptionFunction(option: Options, key: string): Function {
+  const optionFunc = isValidObject(option) ? option[key] : undefined
+  if (optionFunc && typeof optionFunc === 'function') {
+    return optionFunc
+  }
+  return undefined
+}
+
+export const arrayToObject = (array: Array<any>, options?: Options) => {
+  if (!isValidArray(array)) {
     return {}
   }
 
-  if (keyField) {
-    return array.reduce((obj, item) => {
-      obj[item[keyField]] = item
-      return obj
-    }, {})
-  } else {
-    return array.reduce((obj, item) => {
-      if (item || typeof item === 'number') {
-        obj[item] = item
-      }
-      return obj
-    }, {})
-  }
+  const keyFunc = getOptionFunction(options, 'key')
+  const valueFunc = getOptionFunction(options, 'value')
+
+  return array.reduce((obj, item) => {
+    const key = keyFunc ? keyFunc(item) : item
+    const value = valueFunc ? valueFunc(item) : item
+    if (item || typeof item === 'number') {
+      obj[key] = value
+    }
+
+    return obj
+  }, {})
+}
+
+function isValidArray(array: Array<any>) {
+  return Array.isArray(array) && array.length
+}
+
+function isValidObject(object: any): Boolean {
+  return object && typeof object === 'object'
 }
--- test/arr2obj.spec.ts
+++ test/arr2obj.spec.ts
@@ -1,6 +1,6 @@
 import { arrayToObject } from '../src/index'
 
-describe('arr2Obj', () => {
+describe('With basic tests (with argument without errors)', () => {
   it('Empty array should return empty object', () => {
     expect(arrayToObject([])).toEqual({})
   })
@@ -23,5 +23,13 @@ describe('arr2Obj', () => {
 
   it('Should return empty object if all elements of array are null', () => {
     expect(arrayToObject([null])).toEqual({})
+  })
+
+  it('Should return new Key with key function', () => {
+    expect(arrayToObject([1, 2, 3], { key: item => `pre-${item}-post` })).toEqual({
+      'pre-1-post': 1,
+      'pre-2-post': 2,
+      'pre-3-post': 3
+    })
   })
 })
