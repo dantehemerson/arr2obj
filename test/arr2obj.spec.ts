2ac6830f6b4948053196a66bb34559b63fa85c3b
Dante Calderon <mail@dantecalderon.dev>
2019-08-30 18:49:40 -0500

    Add more tests

--- test/arr2obj.spec.ts
+++ test/arr2obj.spec.ts
@@ -32,4 +32,24 @@ describe('With basic tests (with argument without errors)', () => {
       'pre-3-post': 3
     })
   })
+
+  it('Should return new Value with value function', () => {
+    expect(arrayToObject([1, 2, 3], { value: item => `pre-${item}-post` })).toEqual({
+      1: 'pre-1-post',
+      2: 'pre-2-post',
+      3: 'pre-3-post'
+    })
+  })
+
+  it('Should return new Key and Value with custom key and value function', () => {
+    const res = arrayToObject([1, 2, 3], {
+      key: item => `pre-${item}-post`,
+      value: item => Math.pow(item, 2)
+    })
+    expect(res).toEqual({
+      'pre-1-post': 1,
+      'pre-2-post': 4,
+      'pre-3-post': 9
+    })
+  })
 })
